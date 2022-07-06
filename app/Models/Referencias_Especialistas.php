36cf9d45177f0f0dbfe9ade923878e8e1627499b
Leonela Patricia Bermúdez Garcia <LEONELA.BERMUDEZ@ucr.ac.cr>
2022-07-06 00:19:08 -0600

    Primera parte de modelos

--- app/Models/Referencias_Especialistas.php
+++ app/Models/Referencias_Especialistas.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Referencias_Especialistas extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',  
+        'id_Proceso_Intervencion',
+        'referencias',
+        'descripcion',  
+       
+      
+    ];
+
+    protected $id;
+    protected $id_Proceso_Intervencion;
+    protected $referencias;
+    protected $descripcion;
+
+    public function Proceso_Intervencion()
+    {
+        return $this->belongsTo(Proceso_Intervencion::class);
+    }
 }
