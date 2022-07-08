09c0631602e958b75cf19228d745cfea9c0ed17f
Jimenez04 <66088807+Jimenez04@users.noreply.github.com>
2022-07-07 22:26:26 -0600

    Edicion de migraciones

--- database/migrations/2022_07_06_024246_create_revision__solicituds_table.php
+++ database/migrations/2022_07_06_024246_create_revision__solicituds_table.php
@@ -23,15 +23,15 @@ return new class extends Migration
              $table->string('administrador_Cedula',20)->nullable();
              $table->foreign('administrador_Cedula')->references('persona_cedula')->on('administradors');
 
-             $table->string('solicitud_Numero');
+             $table->string('solicitud_Numero')->nullable();
              $table->foreign('solicitud_Numero')->references('numero_solicitud')->on('solicitud_de_adecuacions');
         });
         Schema::table('recomendaciones', function (Blueprint $table) {
-             $table->unsignedBigInteger('revision_Solicitud_id');
+             $table->unsignedBigInteger('revision_Solicitud_id')->nullable();
                 $table->foreign('revision_Solicitud_id')->references('id')->on('revision__solicituds');
         });
         Schema::table('observacions', function (Blueprint $table) {
-             $table->unsignedBigInteger('revision_Solicitud_id');
+             $table->unsignedBigInteger('revision_Solicitud_id')->nullable();
                 $table->foreign('revision_Solicitud_id')->references('id')->on('revision__solicituds');
         });
 
