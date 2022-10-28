84629b16950d97fe70f7a9717193f36ed91fb613
Jimenez04 <66088807+Jimenez04@users.noreply.github.com>
2022-10-28 16:32:05 -0600

    Solución final, bug creación de usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->startingValue(5);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
