ba46dda32c7d78756794008521ef5016d2911f7b
Jimenez04 <66088807+Jimenez04@users.noreply.github.com>
2022-11-13 21:17:52 -0600

    Cambio en vista welcome

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('adequacy');
+    return view('welcome');
     //return view('emails.request.newAdequacy');
 });
 
